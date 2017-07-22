a0ccd91fa3e5b45cfd40459b491282fff6d51287
Alex Norman <alex@x37v.info>
2017-07-22 11:09:48 -0700

    cleaned up and building

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,17 @@
-//use std::collections::HashMap;
-
-/*
-struct Context<'a> {
-  now: u64,
-  items: Vec<& 'a Fn(&mut Context)>
-}
-
-impl<'a> Context<'a> {
-  fn schedule(&mut self, time: u64, func: &Fn(&mut Context)) {
-    println!("now: {}, scheduled time: {}", self.now(), time);
-    func(self);
-  }
-
-  fn now(&self) -> u64 { self.now }
-
-  fn incr(&mut self) -> u64 {
-    self.now = self.now + 1;
-    self.now
-  }
-}
-
-fn blah(c: &mut Context) {
-  println!("blah: {}", c.now());
-}
-
-fn reschedule(c: &mut Context) {
-  let n = c.now() + 10;
-  c.schedule(n, &blah);
-}
-
-fn main() {
-  let mut c = Context{now: 0,items:Vec::new()};
-  let f = |c: &mut Context| {
-    println!("now: {}", c.now());
-  };
-
-  c.items.push(blah);
-  c.schedule(34, &f);
-  c.schedule(35, &f);
-  c.schedule(37, &reschedule);
-  c.schedule(37, &blah);
-  loop {
-    if c.incr() > 64 { break; }
-  }
-}
-*/
-
-
 trait Schedule {
   fn schedule(&mut self, t: u64, f: Box<Fn(&mut Schedule)>) -> ();
   fn now(&self) -> u64;
 }
 
 type SeqFun = Box<Fn(&mut Schedule)>;
-struct Seq {
+
+struct RuntimeSeq {
   items: Vec<SeqFun>
+}
+
+struct Seq {
+  items: Vec<SeqFun>,
+  runtime: RuntimeSeq
 }
 
 impl Schedule for Seq {
@@ -66,24 +23,9 @@ impl Schedule for Seq {
   }
 }
 
-impl Seq {
-  fn new() -> Seq {
-    Seq{items:Vec::new()}
-  }
-
-  fn exec(&self) {
-    let mut c = Seq::new();
-    for f in &self.items {
-      f(&mut c);
-    }
-  }
-}
-
-
-/*
 impl Schedule for RuntimeSeq {
   fn schedule(&mut self, t: u64, f: SeqFun) -> () {
-    //self.items.push(f);
+    self.items.push(f);
   }
   fn now(&self) -> u64 {
     0
@@ -96,16 +38,30 @@ impl RuntimeSeq {
   }
 
   fn exec(&mut self) {
+    let mut c = RuntimeSeq::new();
     for f in &self.items {
-      f(self);
+      f(&mut c);
     }
+    self.items.append(&mut c.items);
   }
 }
 
-*/
+impl Seq {
+  fn new() -> Seq {
+    Seq{items:Vec::new(), runtime: RuntimeSeq::new()}
+  }
+
+  fn exec(&self) {
+    let mut c = RuntimeSeq::new();
+    for f in &self.items {
+      f(&mut c);
+    }
+    c.exec();
+  }
+}
 
 fn doit(context: &mut Schedule) {
-  println!("now: {}", context.now());
+  println!("doit: {}", context.now());
 }
 
 fn doit2(context: &mut Schedule) {
@@ -118,10 +74,10 @@ fn main() {
   c.schedule(23, Box::new(doit));
   c.schedule(34, Box::new(doit2));
   c.schedule(10, Box::new(|context:  &mut Schedule| {
+    println!("outer dude: {}", context.now());
     context.schedule(43, Box::new(|context: &mut Schedule| {
       println!("inner dude");
     }));
-    println!("now: {}", context.now());
   }));
   c.exec();
 }
