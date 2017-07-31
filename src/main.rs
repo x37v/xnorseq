684d7f1bf295529efa5a3d0f63f30a9a80e2ec42
Alex Norman <alex@x37v.info>
2017-07-30 19:27:41 -0700

    builds but doesn't allow inner scheduling yet

--- src/main.rs
+++ src/main.rs
@@ -5,147 +5,97 @@ macro_rules! w {
 }
 
 type TimePoint = u64;
+type Ticks = u64;
+type SchedFun = Fn(&mut Scheduler);
+type SchedFunPtr = Arc<SchedFun>;
 
-trait Schedule {
-  fn schedule(&mut self, t: TimePoint, f: Arc<Fn(&mut Schedule)>) -> ();
+trait Scheduler {
+  fn schedule(&mut self, t: TimePoint, f: SchedFunPtr) -> ();
   fn now(&self) -> TimePoint;
-  fn last(&self) -> TimePoint;
 }
 
-type SeqFun = Arc<Fn(&mut Schedule)>;
-
-struct RuntimeSeq {
-  items: Vec<(TimePoint, SeqFun)>,
-  now: TimePoint,
-  last: TimePoint
+struct Schedule {
+  items: Vec<(TimePoint, SchedFunPtr)>
 }
 
 struct Seq {
-  items: Vec<(TimePoint, SeqFun)>,
-  runtime: RuntimeSeq,
-  now: TimePoint,
-  last: TimePoint
+  schedule: Arc<Schedule>,
+  now: TimePoint
 }
 
-impl Schedule for Seq {
-  fn schedule(&mut self, t: TimePoint, f: SeqFun) -> () {
+impl Schedule {
+  fn new() -> Schedule {
+    Schedule{items: Vec::new()}
+  }
+
+  fn schedule(&mut self, t: TimePoint, f: SchedFunPtr) -> () {
     self.items.push((t, f));
   }
-  fn now(&self) -> TimePoint {
-    self.now
-  }
-  fn last(&self) -> TimePoint {
-    self.last
-  }
-}
 
-impl Schedule for RuntimeSeq {
-  fn schedule(&mut self, t: TimePoint, f: SeqFun) -> () {
-    self.items.push((t, f));
-  }
-  fn now(&self) -> TimePoint {
-    self.now
-  }
-  fn last(&self) -> TimePoint {
-    self.last
-  }
-}
-
-impl RuntimeSeq {
-  fn new() -> RuntimeSeq {
-    RuntimeSeq{items:Vec::new(), now: 0, last: 0}
-  }
-
-  fn exec(&mut self, dur: TimePoint) {
-    let n = self.now() + dur;
-    let l = self.last();
-    self.now = n;
-    let mut v = self.items.clone();
-    let mut iter = v.iter_mut();
-    while let Some(&mut (ref t, ref f)) = iter.next() {
-      let t = *t;
-      if t < n && t > l {
-        self.now = t;
-        f(self);
+  fn items_at(&self, t: TimePoint) -> Vec<SchedFunPtr> {
+    let mut v = Vec::new();
+    for &(ref ft, ref f) in &self.items {
+      if t == *ft {
+        v.push(f.clone())
       }
     }
-    self.now = n;
-    self.last = n;
+    v
+  }
+}
+
+
+impl Scheduler for Seq {
+  fn schedule(&mut self, t: TimePoint, f: SchedFunPtr) -> () {
+    match Arc::get_mut(&mut self.schedule) {
+      Some(s) => { s.schedule(t, f); }
+      None => { println!("ERROR"); }
+    };
+  }
+  
+  fn now(&self) -> TimePoint {
+    self.now
   }
 }
 
 impl Seq {
   fn new() -> Seq {
-    Seq{items:Vec::new(), runtime: RuntimeSeq::new(), now: 0, last: 0}
+    Seq{schedule:Arc::new(Schedule::new()), now: 0}
   }
 
-  fn exec(&mut self, dur: TimePoint) -> TimePoint {
-    let n = self.last() + dur;
-    let l = self.last();
-    self.now = n;
-    for &(ref t, ref f) in &self.items {
-      let t = *t;
-      if t < n && t >= l {
-        self.runtime.now = t;
-        f(&mut self.runtime);
+  fn exec(&mut self, ticks: Ticks) -> () {
+    //context shares the schedule, should allow new additions to schedule
+    let mut context = Seq{now:self.now, schedule: self.schedule.clone()};
+    for _ in 0..ticks {
+      let mut v = self.schedule.items_at(self.now);
+      context.now = self.now;
+      for f in v.iter_mut() {
+        f(&mut context);
       }
-    }
-    self.runtime.exec(dur);
-    self.now = n;
-    self.last = n;
-    n
+      self.now += 1
+    };
   }
 }
 
-fn doit(context: &mut Schedule) {
+fn doit(context: &mut Scheduler) {
   println!("doit: {}", context.now());
 }
 
-fn doit2(context: &mut Schedule) {
-  println!("doit2: {}", context.now());
-  context.schedule(2000, w!(doit));
+fn doit2(context: &mut Scheduler) {
+  let n = context.now();
+  println!("doit2: {}", n);
+  context.schedule(n + 4, w!(doit));
 }
 
-fn midi_note(note: u8, velocity: u8, channel: u8, duration: TimePoint) -> SeqFun {
-  w!(move |context: &mut Schedule| {
-    let off = w!(|context: &mut Schedule| {
-      println!("midi off {}", context.now());
-    });
-    let o = context.now() + duration;
-    println!("midi on {}", context.now());
-    context.schedule(o, off);
-  })
-}
 
 fn main() {
   let mut c = Seq::new();
+  c.schedule(10, w!(move |context: &mut Scheduler| {
+    println!("moved fn {}", context.now());
+  }));
+
   c.schedule(23, w!(doit));
   c.schedule(34, w!(doit2));
-  c.schedule(10, w!(|context: &mut Schedule| {
-    let n = context.now();
-    println!("schedule delay: {}", n);
-    context.schedule(13 + n, w!(|context: &mut Schedule| {
-      println!("delayed {}", context.now());
-    }));
-  }));
-
-  let dfunc = |_context: &mut Schedule| {
-    20
-  };
-
-  c.schedule(20, w!(move |context: &mut Schedule| {
-    let n = context.now() + dfunc(context);
-    println!("schedule delay: {}", n);
-    context.schedule(13 + n, w!(|context: &mut Schedule| {
-      println!("delayed {}", context.now());
-    }));
-  }));
-
-  c.schedule(10, midi_note(12, 127, 1, 23));
-
-  loop {
-    if c.exec(10) > 1000 {
-      break
-    }
-  }
+  c.exec(20);
+  c.exec(20);
 }
+
